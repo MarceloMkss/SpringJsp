d78e21badb7719d450a7b5a1696cf3376aaeb55a
Marce <Marce@DESKTOP-U61ABJ1>
2021-02-25 08:19:33 +0100

    Inicio de springmvcjsp SIN Thymeleaf y con Spring Web y Dev Tools

---
+++ springjsp/src/test/java/com/mkss/infotek/SpringjspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkss.infotek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
